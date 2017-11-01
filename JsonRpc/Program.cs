bab349b8bd641541c9402134f4677d83bc48ad75
Lo5t <samurai.mit@gmail.com>
2017-11-01 18:03:49 +0100

    inital commit

---
+++ JsonRpc/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+
+namespace JsonRpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint serviceEndpoint = GetIPEndPointFromHostName("", 32, false);
+            JsonRpcClient jsonRpcClient = new JsonRpcClient(serviceEndpoint, Encoding.ASCII);
+
+        }
+
+        public static IPEndPoint GetIPEndPointFromHostName(string hostName, int port, bool throwIfMoreThanOneIP)
+        {
+            var addresses = System.Net.Dns.GetHostAddresses(hostName);
+            if (addresses.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Unable to retrieve address from specified host name.",
+                    "hostName"
+                );
+            }
+            else if (throwIfMoreThanOneIP && addresses.Length > 1)
+            {
+                throw new ArgumentException(
+                    "There is more that one IP address to the specified host.",
+                    "hostName"
+                );
+            }
+            return new IPEndPoint(addresses[0], port); // Port gets validated here.
+        }
+
+        // Handles IPv4 and IPv6 notation.
+        public static IPEndPoint CreateIPEndPoint(string endPoint)
+        {
+            string[] ep = endPoint.Split(':');
+            if (ep.Length < 2) throw new FormatException("Invalid endpoint format");
+            IPAddress ip;
+            if (ep.Length > 2)
+            {
+                if (!IPAddress.TryParse(string.Join(":", ep, 0, ep.Length - 1), out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+            else
+            {
+                if (!IPAddress.TryParse(ep[0], out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+            int port;
+            if (!int.TryParse(ep[ep.Length - 1], NumberStyles.None, NumberFormatInfo.CurrentInfo, out port))
+            {
+                throw new FormatException("Invalid port");
+            }
+            return new IPEndPoint(ip, port);
+        }
+    }
+}
